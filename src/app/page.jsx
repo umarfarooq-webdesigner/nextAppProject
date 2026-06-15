d1dcba58634201feefbfca91f4021b26cec7d3b3
Umar Farooq <umarfarooq249256@gmail.com>
2026-06-15 20:30:18 +0500

    new

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <div className="w-full min-h-screen grid justify-center items-center text-center">
-        <h1 className="text-2xl">Welcome To Home</h1>
+      <div className="w-full min-h-screen grid justify-center items-center text-center bg-olive-400">
+        <h1 className="text-2xl ">Welcome To Home</h1>
       </div>
     </>
   );
