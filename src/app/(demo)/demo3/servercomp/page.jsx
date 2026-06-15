e8ca9147c72cc1452484e6640f20ee9237a92e42
Umar Farooq <umarfarooq249256@gmail.com>
2026-06-15 20:01:54 +0500

    change

--- src/app/(demo)/demo3/servercomp/page.jsx
+++ src/app/(demo)/demo3/servercomp/page.jsx
@@ -1,41 +1,41 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-export const metadata = {
-  title: "Server component Page",
-};
+// export const metadata = {
+//   title: "Server component Page",
+// };
 
-const ServerComp = async () => {
-  const res = await fetch(process.env.API_URL);
+// const ServerComp = async () => {
+//   const res = await fetch(process.env.API_URL);
 
-  const data = await res.json();
-  console.log(data);
+//   const data = await res.json();
+//   console.log(data);
 
-  return (
-    <>
-      <div className=" justify-center text-center items-center">
-        <h1>Server Components Page</h1>
+//   return (
+//     <>
+//       <div className=" justify-center text-center items-center">
+//         <h1>Server Components Page</h1>
 
-        <div className="my-5">
-          <Link
-            href={"/demo3"}
-            className="px-5 py-4 rounded capitalize bg-amber-600 text-white"
-          >
-            back
-          </Link>
-        </div>
+//         <div className="my-5">
+//           <Link
+//             href={"/demo3"}
+//             className="px-5 py-4 rounded capitalize bg-amber-600 text-white"
+//           >
+//             back
+//           </Link>
+//         </div>
 
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-10 py-5 ">
-          {data.map(({ id, name, username, email }) => (
-            <div className="border bg-gray-100 p-4 rounded" key={id}>
-              <h1>{name}</h1>
-              <h2>{username}</h2>
-              <p>{email}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
-};
+//         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-10 py-5 ">
+//           {data.map(({ id, name, username, email }) => (
+//             <div className="border bg-gray-100 p-4 rounded" key={id}>
+//               <h1>{name}</h1>
+//               <h2>{username}</h2>
+//               <p>{email}</p>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
-export default ServerComp;
+// export default ServerComp;
