5ea00d7336d40734f3460cba54c32b26f4dc3ee8
Umar Farooq <umarfarooq249256@gmail.com>
2026-06-15 20:06:28 +0500

    new

--- src/app/(demo)/demo3/servercomp/loading.jsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gray-100">
-      <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600" />
-      <p className="text-sm font-medium text-gray-600">Loading...</p>
-    </div>
-  );
-}
