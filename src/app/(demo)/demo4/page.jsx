5e173b01bea8a4394660d781e5c242f214a2d1e3
Umar Farooq <umarfarooq249256@gmail.com>
2026-06-17 10:59:59 +0500

    new

---
+++ src/app/(demo)/demo4/page.jsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Demo4",
+};
+
+const Demo4 = () => {
+  return (
+    <div className="flex flex-col justify-center items-center py-10 px-5 ">
+      <h1>📅 Day 18 — MongoDB Setup (Next.js + Database)</h1>
+    </div>
+  );
+};
+
+export default Demo4;
---
+++ src/models/User.jsx
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+  id: Number,
+  username: String,
+  name: String,
+  email: String,
+  address: String,
+  phone: Number,
+});
+
+export default mongoose.models.Users || mongoose.model("Users", UserSchema);
