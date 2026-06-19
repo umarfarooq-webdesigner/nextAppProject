87a52cd285ec90d0ce58925e3d304e4eb91cf1fd
Umar Farooq <umarfarooq249256@gmail.com>
2026-06-20 02:35:05 +0500

    new

--- src/app/(demo)/demo4/page.jsx
+++ src/app/(demo)/demo4/page.jsx
@@ -5,7 +5,7 @@ export const metadata = {
 const Demo4 = () => {
   return (
     <div className="flex flex-col justify-center items-center py-10 px-5 ">
-      <h1>📅 Day 18 — MongoDB Setup (Next.js + Database)</h1>
+      <h1>📅 Day 18 — MongoDB Setup </h1>
     </div>
   );
 };
